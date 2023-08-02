b2e36e2adc63ee6b7145f3c53a5454c498505b16
udita.chakraborty <mail2udita@yahoo.com>
2023-08-02 11:49:53 +0200

    Create Leave Type Command

---
+++ HR.LeaveManagement.Clean/HR.Leavemanagement.Application/Features/Commands/CreateLeaveType/CreateLeaveTypeCommand.cs
@@ -0,0 +1,18 @@
+﻿using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HR.Leavemanagement.Application.Features.Commands.CreateLeaveType
+{
+    // we are returning the id of the record that was created
+    public class CreateLeaveTypeCommand :IRequest<int>
+    {
+        public string Name { get; set; } = string.Empty;
+
+        public string Description { get; set; }
+
+    }
+}
